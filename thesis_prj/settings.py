188e5a23fd53358d57ef84e56c29329c1ef0363c
Mahesh Thapa <mahesh.gru03@gmail.com>
2019-02-21 17:14:41 +0100

    db

--- thesis_prj/settings.py
+++ thesis_prj/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'thesis_prj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '`reflection`',
-        'USER': 'reflect_user',
-        'PASSWORD': 'reflect1on',
-        'HOST': 'www.db4free.net',
+        'NAME': 'heroku_e53413dd171eebd',
+        'USER': 'bf276f67becee6',
+        'PASSWORD': '9d226104',
+        'HOST': 'eu-cdbr-west-02.cleardb.net',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode = 'STRICT_ALL_TABLES';",
